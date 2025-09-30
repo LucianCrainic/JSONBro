40b298a8b849d4050d22b3be11e886e7c5aba06d
ldcrainic <luciandorin.crainic@elt.it>
2025-09-30 16:41:54 +0200

    feat: register activity bar data provider in extension activation

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,15 @@
 import * as vscode from 'vscode';
 import { CommandHandler } from './commands/command-handler';
+import { JSONBroActivityBarProvider } from './activity-bar-provider';
 
 export function activate(context: vscode.ExtensionContext) {
     console.log('JSONBro extension is now active!');
     
-    const commandHandler = new CommandHandler(context);
+    // Register the activity bar tree data provider
+    const activityBarProvider = new JSONBroActivityBarProvider();
+    vscode.window.registerTreeDataProvider('jsonbro.explorer', activityBarProvider);
+    
+    const commandHandler = new CommandHandler(context, activityBarProvider);
     commandHandler.registerCommands(context);
 }
 
